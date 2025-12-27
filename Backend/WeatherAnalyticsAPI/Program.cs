8ea922172b6532507681ef77ca14a4103958f1e0
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-12-27 15:07:42 +0530

    begin the ui with React vite

--- Backend/WeatherAnalyticsAPI/Program.cs
+++ Backend/WeatherAnalyticsAPI/Program.cs
@@ -4,6 +4,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins(
+                "http://localhost:5173", 
+                "http://localhost:5174", 
+                "http://localhost:3000",
+                "http://localhost:5010"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddMemoryCache();
 builder.Services.AddHttpClient();
 
@@ -12,6 +29,9 @@ builder.Services.AddScoped<ComfortIndexService>();
 builder.Services.AddScoped<CacheService>();
 
 var app = builder.Build();
+
+// Use CORS
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 app.MapControllers();
